3e4ae8af78abdc6102dd4521c9d9b2fb56f68c29
dollarstir <you@example.com>
2019-07-18 11:29:00 +0000

    dollar

--- myapp.py
+++ myapp.py
@@ -1,5 +1,6 @@
 from flask import *
 import json
+import os
 
 app = Flask(__name__) 
 getlist =[]
@@ -59,4 +60,5 @@ def course():
 
 
 if __name__ =='__main__':
-    app.run(debug=True, host='0.0.0.0')
+     port = os.environ.get('PORT', 5000)
+    app.run(debug=True, host='0.0.0.0',port=port)
